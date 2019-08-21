e71a999ec1d6ea0dad758cf6c33c65c37c07d4cb
Deepak Singh <deepaksingh@saffrontech.net>
2019-08-21 12:09:10 +0530

    store location

--- app/etc/config.php
+++ app/etc/config.php
@@ -6,6 +6,7 @@ return [
         'Magento_Eav' => 1,
         'Magento_Theme' => 1,
         'Magento_Cms' => 1,
+        'Ideo_StoreLocator' => 1,
         'Magento_Customer' => 1,
         'Magento_AdminNotification' => 1,
         'Magento_Indexer' => 1,
