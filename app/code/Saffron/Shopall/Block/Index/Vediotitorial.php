2bb603940e4e372bba86f3b6605619e209196d44
Deepak Singh <deepaksingh@saffrontech.net>
2019-10-03 17:19:36 +0530

    popup

---
+++ app/code/Saffron/Shopall/Block/Index/Vediotitorial.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Saffron\Shopall\Block\Index;
+
+class Vediotitorial extends \Magento\Framework\View\Element\Template{
+
+
+public function getIfram(){
+	
+		
+	$id  = $_GET['vediourl'];
+	 if(!empty($id)){	
+	 
+	 }else{
+		 
+		 
+	 }
+		//echo ;
+		
+	return  $id;
+	
+}
+
+
+
+
+}
