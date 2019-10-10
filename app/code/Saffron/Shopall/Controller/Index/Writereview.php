6a27cc2d40bc4d8f5d6c7f4fab2fb94f94fddf48
Deepak Singh <deepaksingh@saffrontech.net>
2019-10-10 18:45:23 +0530

    donet upadte

---
+++ app/code/Saffron/Shopall/Controller/Index/Writereview.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Saffron\Shopall\Controller\Index;
+
+class Writereview extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+    
+	$this->_redirect('thank-you/');
+	
+	}
+}
