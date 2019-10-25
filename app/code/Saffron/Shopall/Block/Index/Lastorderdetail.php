7507c5105613c284a4cd5f5f025b8196144e58ad
Deepak Singh <deepaksingh@saffrontech.net>
2019-10-25 17:40:46 +0530

    read me

--- app/code/Saffron/Shopall/Block/Index/Lastorderdetail.php
+++ app/code/Saffron/Shopall/Block/Index/Lastorderdetail.php
@@ -82,6 +82,7 @@ $orderCollection = $OrderFactory->create()->addFieldToSelect(array('*'));
  $orderCollection->addFieldToFilter('created_at', ['lteq' => $now->format('Y-m-d H:i:s')])->addFieldToFilter('created_at', ['gteq' => $now->format($prev_date)]);
 $alloder = $orderCollection->getData();
 $i=0;
+if(count($alloder)>0){
  foreach($alloder as $items){
   $orders[$i]['increment_id']	= $items['increment_id']; 
  $i++; } 
@@ -91,6 +92,9 @@ $randIndex = array_rand($arrX);
 return $arrX[$randIndex];
 
 	
+}else{
+	
+	$arrX[];
 }
 
 
