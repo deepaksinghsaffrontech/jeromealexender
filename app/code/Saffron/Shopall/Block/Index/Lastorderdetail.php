8e070c285cd7e0dde44e696c71df4f81a8588ac6
govind <govind.bisht@saffrontech.net>
2019-11-12 17:40:17 +0530

    css

--- app/code/Saffron/Shopall/Block/Index/Lastorderdetail.php
+++ app/code/Saffron/Shopall/Block/Index/Lastorderdetail.php
@@ -73,7 +73,7 @@ $orders =array();
 $now = new \DateTime();
 
 $date = date('m/d/Y h:i:s', time());
-$prev_date = date('Y-m-d h:i:s', strtotime($date .' -90 day'));
+$prev_date = date('Y-m-d h:i:s', strtotime($date .' -5 day'));
 
 
 $objectManager = \Magento\Framework\App\ObjectManager::getInstance();
